290da32b5cd213c7dc660bb199eb78490724a1bd
Hikibi <615973075@qq.com>
2018-02-05 08:33:55 +0800

    18/2/5 8.33 加入test类, 展开文件夹的上传

---
+++ src/main/java/com/yanjiang/researchRA/controller/ResearchRAController.java
@@ -0,0 +1,13 @@
+package com.yanjiang.researchRA.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by dllo on 18/2/5.
+ */
+@Controller("/RA")
+public class ResearchRAController {
+
+
+
+}
---
+++ src/main/java/com/yanjiang/researchRA/domain/test.java
@@ -0,0 +1,7 @@
+package com.yanjiang.researchRA.domain;
+
+/**
+ * Created by dllo on 18/2/5.
+ */
+public class test {
+}
